808d16b04ac8bee8dcffbdb6df1e165bc4c521a9
anthonynarine <anarine83@gmail.com>
2022-11-18 18:09:16 -0500

    Updated App component to send Data to ProfileCard Compenent

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
-
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+//when making custom components we get to choose the names eg title & handle
+            <ProfileCard title="Siri" handle="@Siri6" />
+            <ProfileCard title="Alexa" handle="@Alexa23" />
+            <ProfileCard title="Cortana" handle="@cortana8" />
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,23 @@
-function ProfileCard() {
-    return <div>Profile Card</div>
+function ProfileCard(props) {
+    const title = props.title  
+    const handle = props.handle
+
+    return (
+        <div>
+            {/* <div>Title is {props.title}</div>
+            <div>Handle is {props.handle}</div> */} 
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    )
 }
 
 export default ProfileCard;
+
+//step 3 in the props system is to have ProfileCard receive
+// the arguments passed into App (title = "siri" handle= @siri etc)
+//props will act like a place holder for the data coming from apps
+//we use dot notation to access the data props.title, props.handle
+
+//we can also save the data in variables and call them in our component
+// function as show above.
